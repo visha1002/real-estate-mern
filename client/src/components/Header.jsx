4d17be2a2d19b640157edfbe6feabb1ab7d13c09
Visha <vishadobariya1002@gmail.com>
2024-08-04 12:24:06 +0530

    Added Header Component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-teal-700">Earth</span>
+            <span className="text-green-900">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-50 p-2 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="search here..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-teal-700 hover:text-fuchsia-900 cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-teal-700 hover:text-fuchsia-900 cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="text-teal-700 hover:text-fuchsia-900 cursor-pointer">
+              Sign In
+            </li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
